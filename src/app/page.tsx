121394fd1fad843f06fb9cb0aef8166a8fc80824
Abrarul Islam <61227893+MdAbrar585@users.noreply.github.com>
2023-06-03 16:59:09 +0600

    redux-toolkit initial practice done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from 'next/image'
+'use client';
+
+import type { RootState } from "@/GlobalRedux/store";
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import { increment, decrement, incrementByAmmount } from "@/GlobalRedux/Features/counter/counterSlice";
 
 export default function Home() {
+
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <main>
-      
+    <main className="flex flex-col m-10">
+      <button 
+        className="rounded-full bg-teal-500 p-5"
+        onClick={() => dispatch(increment())}
+      >Increment</button>
+      <span className="text-center">{count}</span>
+      <button 
+        className="rounded-full bg-teal-500 p-5 mt-2"
+        onClick={() => dispatch(decrement())}
+      >Decrement</button>
+      <button 
+        className="rounded-full bg-teal-500 p-5 mt-2"
+        onClick={() => dispatch(incrementByAmmount(2))}
+      >Increment by 2</button>
     </main>
   )
 }
